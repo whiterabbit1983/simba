08d572665d06c314c3030974ac21a7cd8078220f
Diwank Singh <diwank.singh@gmail.com>
2017-10-18 00:01:00 -0700

    lein new
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simba
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/simba/core_test.clj
@@ -0,0 +1,7 @@
+(ns simba.core-test
+  (:require [clojure.test :refer :all]
+            [simba.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
