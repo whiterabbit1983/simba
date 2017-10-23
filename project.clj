551aac4f2f91b5ffafb38d8785021e72c795b05d
bhaskarsaraogi <bhaskar.saraogi@gmail.com>
2017-10-23 15:57:49 -0700

    upgrade clojure version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :main simba.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
                  [amazonica "0.3.113"]
                  [buddy/buddy-core "1.4.0"]
                  ;; [cheshire "5.8.0"]
@@ -17,4 +17,5 @@
                  [im.chit/hara.expression.form "2.5.10"]
                  [im.chit/hara.function.args "2.5.10"]
                  [pinpointer "0.1.0-SNAPSHOT"]
-                 [metosin/spec-tools "0.5.0"]])
+                 [metosin/spec-tools "0.5.0"]]
+  :jvm-opts ^:replace [])
