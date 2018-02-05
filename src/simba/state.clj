7f3c4f40fe09b8a890bdcf9fb904bd4cc3ea1993
Dmitry Paramonov <dmitry.paramonov@scramteam.com>
2018-02-05 21:52:09 +0300

    phase-1: get-capacity and get-available functions fixed

--- src/simba/state.clj
+++ src/simba/state.clj
@@ -12,9 +12,9 @@
   (sqs/get-queue-attributes q ["All"]))
 
 (defn get-capacity [q]
-  (let [attrs (q get-attrs)
+  (let [attrs (get-attrs q)
         key :ApproximateNumberOfMessages
-        capacity (key q)]
+        capacity (read-string (key attrs))]
 
     (log/debug "Capacity: " (str capacity))
     capacity))
@@ -24,21 +24,21 @@
   (let [q-urns (map :sqs-urn workers)
         qs (map sqs/find-queue q-urns)
         all-found? (empty? (filter nil? qs))
-        current-state (and all-found? (map get-capacity qs))]
+        current-state (into [] (and all-found? (map get-capacity qs)))]
 
     (if-not all-found?
       (error "Worker queues not found"))
 
-    (remove nil? (->> workers (map-indexed
-     (fn [i worker]
-       (let [current (get current-state i)
-             capacity (:capacity worker)
-             hwm (:hwm worker)
-             above-hwm? (> current hwm)
-             available? (> capacity current)]
+    (into [] (remove nil? (->> workers (map-indexed
+                                        (fn [i worker]
+                                          (let [current (get current-state i)
+                                                capacity (:capacity worker)
+                                                hwm (:hwm worker)
+                                                above-hwm? (> current hwm)
+                                                available? (> capacity current)]
 
-         (log/info "Getting current state")
-         (and available?
-              (assoc worker
-                :current current
-                :above-hwm? above-hwm?)))))))))
+                                            (log/info "Getting current state")
+                                            (and available?
+                                                 (assoc worker
+                                                   :current current
+                                                   :above-hwm? above-hwm?))))))))))
