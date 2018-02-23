f37b5f6f5251962b14fbe1d8cb70a93e2803d728
Dmitry Paramonov <dmitry.paramonov@scramteam.com>
2018-02-23 18:35:32 +0300

    ampq: dependencies to work with ActiveMQ

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-
                  [amazonica "0.3.118"]
                  [buddy/buddy-core "1.4.0"]
                  [com.cemerick/bandalore "0.0.6" :exclusions [joda-time]]
@@ -17,5 +16,8 @@
                  [io.forward/yaml "1.0.6"]
                  [metosin/spec-tools "0.6.0-SNAPSHOT"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [pinpointer "0.1.0"]]
+                 [pinpointer "0.1.0"]
+                 [org.apache.activemq/activemq-client "5.15.0"]
+                 [org.apache.activemq/activemq-pool "5.15.0"]
+                 [javax.jms/jms-api "1.1-rev-1"]]
   :jvm-opts ^:replace [])
